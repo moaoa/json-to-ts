a1103808bf7c95171ce4a25074ba1e0a34d5b89c
moaad <moaadbn3@gmail.com>
2023-12-23 19:26:46 +0200

    json to typescript interface with codemirror

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React from "react";
+import logo from "@assets/img/logo.svg";
+import { twMerge } from "tailwind-merge";
+import JsonToTS from "json-to-ts";
+import {
+  Controlled as CodeMirror,
+  UnControlled as ReadOnlyEditor,
+} from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/javascript/javascript";
+// note: you must install codemirror@version5 for this to work
 
 export default function Popup(): JSX.Element {
+  const [tabIndex, setTabIndex] = React.useState(0);
+  const [result, setResult] = React.useState("");
+  const [jsonInputValue, setJsonInputValue] = React.useState("");
+
+  const convert = (json: string) => {
+    try {
+      const res = JSON.parse(json);
+      setResult(JsonToTS(res)?.[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleJsonInputChange = (value: string) => {
+    setJsonInputValue(value);
+    console.log(convert(value));
+  };
+  const tabs = ["JSON", "Result"];
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
+      {/* start: tabs */}
+      <ul className="flex flex-wrap text-sm font-medium text-center  border-b border-gray-700 text-gray-400">
+        {tabs.map((tab, index) => (
+          <li
+            key={tab}
+            className={twMerge(
+              "inline-block p-4  rounded-t-lg cursor-pointer hover:bg-gray-700 hover:text-gray-300",
+              index === tabIndex &&
+                "bg-blue-700 text-blue-300 hover:bg-blue-600 hover:text-blue"
+            )}
+            onClick={() => setTabIndex(index)}
+          >
+            {tab}
+          </li>
+        ))}
+      </ul>
+      {/* end: tabs */}
+
+      {/* start: content */}
+      <div className="flex p-4">
+        {tabIndex === 0 && (
+          <div className="text-left w-full">
+            <CodeMirror
+              value={jsonInputValue}
+              options={{
+                lineNumbers: true,
+                mode: "javascript",
+                theme: "material",
+              }}
+              onBeforeChange={(editor, data, value) => {
+                handleJsonInputChange(value);
+              }}
+            />
+          </div>
+        )}
+        {tabIndex === 1 && (
+          <div className="text-left w-full">
+            <ReadOnlyEditor
+              value={result}
+              options={{
+                lineNumbers: true,
+                theme: "dark",
+              }}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       animation: {
-        'spin-slow': 'spin 20s linear infinite',
-      }
+        "spin-slow": "spin 20s linear infinite",
+      },
     },
   },
-  prefix: '',
+  prefix: "",
   plugins: [],
-}
+};
